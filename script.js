6ccc18a24a31faf9e0ea3e3ac61c4b23565963a8
Owen <owenmccr@gmail.com>
2023-09-23 17:20:53 -0400

    removed some code and added comments

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 var today = dayjs();
 var currentHour = dayjs().format("H");
-console.log(currentHour);
 
 $(function () {
   var scheduleEl = $("#schedule");
 
+  // Saving the user input to localstorage and using the id value of the current element as the key
   $(".saveBtn").on("click", function () {
     var description = $(this).siblings(".description").val();
     var hourID = $(this).parent("div").attr("id");
     localStorage.setItem(hourID, description);
   });
 
+  // Looping through each div to grab the time, compare it to the current time, and change the class. We also display any saved descriptions from localstorage here
   scheduleEl.children("div").each(function () {
     var parentId = $(this).attr("id");
     var parentHour = parseInt(parentId.split("-")[1]);
@@ -24,11 +25,11 @@ $(function () {
       $(this).addClass("past");
     }
 
-
     if (storedDescription !== null) {
       $(this).children(".description").text(storedDescription);
     }
   });
 
+  // Displaying the current day
   $("#currentDay").text(today.format("dddd, MMM D"));
 });
